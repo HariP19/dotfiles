e2826f1dca1ce48d22137191cf4a237b2f9026a3
Hari <hari.191197@gmail.com>
2023-04-29 19:40:01 +0800

    add install script for essential tools

---
+++ scripts/install_essentials.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create and change to the 'install' directory
+mkdir install
+cd install
+
+# Install LSD
+curl -L -o lsd_0.23.1_amd64.deb "https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb"
+sudo dpkg -i lsd_0.23.1_amd64.deb
+rm lsd_0.23.1_amd64.deb
+
+# Install Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Install Zoxide
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ajeetdsouza/zoxide/master/install.sh | sh
+
+# Change back to the previous directory
+cd ..
